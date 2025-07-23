f047fa931dfbc91bcf3c6adc5f3a24cb1e60d78a
Catherine Vuthi <vuthi.esther@gmail.com>
2025-07-23 10:54:30 +0300

    Update main layout and page

--- mpesa-expense-tracker/src/app/page.tsx
+++ mpesa-expense-tracker/src/app/page.tsx
@@ -1,9 +1,14 @@
+import Cta from './components/Cta';
+import Demo from './components/Demo';
 import Hero from './components/Hero';
+import Pricing from './components/Pricing';
 
 export default function Home() {
 	return (
 		<div className="font-sans flex flex-col min-h-screen p-6 pb-20 gap-16">
 			<Hero />
+			<Demo />
+			<Pricing />
 		</div>
 	);
 }
