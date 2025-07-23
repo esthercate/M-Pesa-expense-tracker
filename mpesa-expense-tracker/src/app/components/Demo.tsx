006cc1b19a27ca70606372c7dc0b7f4310625db4
Catherine Vuthi <vuthi.esther@gmail.com>
2025-07-23 10:39:11 +0300

    Demo component

---
+++ mpesa-expense-tracker/src/app/components/Demo.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Props = {}
+
+const Demo = (props: Props) => {
+  return (
+    <div id='demo' className="w-full max-w-7xl mx-auto mt-2 md:mt-10 px-0 md:px-4">
+			<div className="text-center mb-8">
+				<h1 className="text-lg md:text-3xl font-bold mb-2">
+					How it Works!
+				</h1>
+      </div>
+      <div>
+        Process of generating pdf statement form m-pesa apps
+      </div>
+    </div>
+  )
+}
+
+export default Demo
