841ed7096c046c6e70c26f422da6f03c2fdeef02
Catherine Vuthi <vuthi.esther@gmail.com>
2025-07-24 11:06:17 +0300

    Add required packages and update config

--- mpesa-expense-tracker/next.config.ts
+++ mpesa-expense-tracker/next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
+/** @type {NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+	webpack(config) {
+		// Allow .mjs files
+		config.module.rules.push({
+			test: /\.mjs$/,
+			type: 'javascript/auto',
+		});
+
+		// Treat PDF worker as an asset (so ?url works)
+		config.module.rules.push({
+			test: /pdf\.worker\.mjs$/,
+			type: 'asset/resource',
+		});
+
+		return config;
+	},
 };
 
 export default nextConfig;
