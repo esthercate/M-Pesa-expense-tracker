e4fbe30e2c1db572a4349189ab9fc99fb9afe247
Catherine Vuthi <vuthi.esther@gmail.com>
2025-10-02 20:31:33 +0300

    Update footer

--- mpesa-expense-tracker/src/app/components/Footer.tsx
+++ mpesa-expense-tracker/src/app/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = (props: Props) => {
 
 	return (
 		<>
-			<footer className="w-full px-4 md:px-10 py-4 flex flex-col sm:flex-row justify-between items-center text-sm text-gray-600 border-t border-gray-300">
+			<footer className="w-full px-4 md:px-10 py-4 flex flex-col gap-3 justify-between items-center text-sm text-gray-600 border-t border-gray-300">
 				<p className="mb-2 sm:mb-0 text-center">
 					Copyright © 2025 M-pesa Expense Tracker.
 				</p>
@@ -20,7 +20,7 @@ const Footer = (props: Props) => {
 					>
 						Privacy
 					</button>
-					<a
+					{/* <a
 						href="/login"
 						className="text-blue-600 transition cursor-pointer"
 					>
@@ -31,7 +31,7 @@ const Footer = (props: Props) => {
 						className="cursor-pointer text-blue-600 transition"
 					>
 						Signup
-					</a>
+					</a> */}
 				</div>
 			</footer>
 
